a914a1b2bf128e8069cd1e18dd961387152aa051
Anthony Graniero <ananfro@gmail.com>
2019-12-15 18:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+class Polygon{
+  constructor(sidesArray){
+    this.sidesArray = sidesArray;
+  }
+  
+  get countSides(){
+    return this.sidesArray.length;
+  }
+  
+  get perimeter(){
+    
+  }
+  
+  // get perimeter() {
+  //   if (!Array.isArray(this.sides)) return;
+  //   let sum = 0;
+  //   for (var int of this.sides) {
+  //     sum += int
+  //   }
+  //   return sum
+  // }
+  
+}
